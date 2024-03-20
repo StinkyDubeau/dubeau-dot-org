e3caa0f3d255ded14eb749f5111aa9817ae1e94a
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-20 08:26:35 -0400

    Fix centering on ultrawide displays

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -2,7 +2,7 @@ export default function Frame(props) {
   return (
     <>
       <div className="bg-zinc-800 min-h-screen">
-        <div className="max-w-screen-xl p-4">{props.children}</div>
+        <div className="max-w-screen-xl m-auto p-4">{props.children}</div>
       </div>
     </>
   );
