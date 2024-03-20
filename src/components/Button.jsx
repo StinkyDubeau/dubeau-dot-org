8ca4f7d8df93c0ec9c34155db88ebf2c6b2a4223
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-20 10:04:35 -0400

    Fix buggy buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
 export default function Button(props) {
   return (
-    <button className="rounded-full overflow-hidden hover:border-b-4 hover:border-2 hover:border-slate-400 hover:border-b-slate-700 h-16 w-16 transition-all hover:bg-slate-500">
-      {props.children}
-      <p className="text-slate-200 text-xs mt-1 font-header">{props.text}</p>
-    </button>
+    <a
+      href={props.to}
+      target="_blank"
+    >
+      <button className="rounded-full overflow-hidden hover:border-b-4 hover:border-2 hover:border-slate-400 hover:border-b-slate-700 h-16 w-16 transition-all hover:bg-slate-500">
+        {props.children}
+        <p className="text-slate-200 text-xs mt-1 font-header">{props.text}</p>
+      </button>
+    </a>
   );
 }
