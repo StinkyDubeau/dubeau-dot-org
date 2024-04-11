f6d4f0e9a1544da0054540441202cc822790f669
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-11 13:21:24 -0400

    Add big button component

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -2,7 +2,14 @@ export default function Frame(props) {
   return (
     <>
       <div className="bg-zinc-800 min-h-screen">
-        <div className="max-w-screen-xl m-auto p-4">{props.children}</div>
+        <div className="max-w-screen-xl m-auto p-4">
+          {" "}
+          <div className="flex justify-center -mt-24">
+            <div className="flex flex-col justify-center h-screen">
+              {props.children}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/Servers.jsx
+++ src/pages/Servers.jsx
@@ -1,12 +1,21 @@
 import Panel from "../components/Panel";
+import Frame from "../components/Frame";
+import Button from "../components/BigButton";
+import { Link } from "react-router-dom";
 
 export default function Servers(props) {
   return (
-    <>
+    <Frame>
       <Panel>
-        <p>Out of order, sorry /:</p>
-        <p>Join the <a href="https://discord.gg/n5dRq7c3Rv">discord</a> for updates.</p>
+        <p className="text-white font-regular">Out of order, sorry /:</p>
+        <p className="text-white font-regular">
+          Join the <a href="https://discord.gg/n5dRq7c3Rv">discord</a> for
+          updates.
+        </p>
       </Panel>
-    </>
+      <Panel>
+        <Link className="font-regular text-white" to="/"><Button>Back to home</Button></Link>
+      </Panel>
+    </Frame>
   );
 }
