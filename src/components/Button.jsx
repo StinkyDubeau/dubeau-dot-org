3b6690c29850f28edaa0177f3215d7b3ea110783
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-11 13:24:08 -0400

    Fix nested <a> tags. Replace them with react router <Link> components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,8 @@
 export default function Button(props) {
   return (
-    <a
-      href={props.to}
-      target="_blank"
-    >
-      <button className="rounded-full overflow-hidden hover:border-b-4 hover:border-2 hover:border-green-400 hover:border-b-green-700 h-16 w-16 transition-all hover:bg-green-500">
+      <button className="rounded-full overflow-hidden hover:border-b-4 hover:border-2 hover:border-green-400 hover:border-b-green-700 h-12 transition-all hover:bg-green-500">
         {props.children}
         <p className="text-green-200 text-xs mt-1 font-header">{props.text}</p>
       </button>
-    </a>
   );
 }
