549b754b6664f0b29a71abeba89f9253f6a3d695
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-11 20:59:48 -0400

    Add server cards

--- src/components/ServerCard.jsx
+++ src/components/ServerCard.jsx
@@ -5,12 +5,15 @@ export default function ServerCard(props) {
         <>
             <div className="card h-44 w-36 overflow-hidden rounded-xl bg-slate-50 shadow-lg transition-all hover:scale-105 hover:shadow-xl">
                 <Link to={props.to}>
-                    <img className="object-cover h-full" src={props.img} />
-                    <div className="relative top-[-60%]">
-                        <p className="font-headerScript text-3xl text-white">
+                    <img className="h-full object-cover" src={props.img} />
+                    {/* Container */}
+                    <div className="relative top-[-100%] m-2 h-40 rounded-lg border-white transition-all ">
+                        {/* Spacer */}
+                        <div className="h-12" />
+                        <p className="font-headerScript text-3xl text-white drop-shadow">
                             {props.title}
                         </p>
-                        <p className="text-white font-header">
+                        <p className="font-header text-white drop-shadow">
                             {props.subtitle}
                         </p>
                     </div>
--- src/pages/Servers.jsx
+++ src/pages/Servers.jsx
@@ -2,17 +2,48 @@ import Panel from "../components/Panel";
 import Frame from "../components/Frame";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
-import ModdedBanner from "../assets/modded-banner.png";
 import ServerCard from "../components/ServerCard";
+
+import ModdedBanner from "../assets/modded-banner.png";
+import BetaBanner from "../assets/beta-banner.webp";
+import ModernBanner from "../assets/modern-banner.jpeg";
+import FactorioBanner from "../assets/factorio-banner.jpg";
+import RotnBanner from "../assets/rotn-banner.png"
 
 export default function Servers(props) {
     return (
         <Frame>
             <div className="grid">
                 <div className="w-screen">
+                    <div>
+                        <p>I run some game servers</p>
+                    </div>
                     {/* Gradient bg */}
-                    <div class="animate-gradient-x grid items-center justify-center gap-2 bg-gradient-to-br from-blue-400 via-cyan-400 to-green-400 p-2">
-                        <ServerCard title="Modded" subtitle="Create mod" img={ModdedBanner} to="/modded" />
+                    <div className="animate-gradient-x m-5 flex flex-wrap justify-center gap-8 rounded-lg bg-gradient-to-br from-blue-400 via-cyan-400 to-green-400 p-4">
+                        <ServerCard
+                            title="RotN"
+                            subtitle="Modded Minecraft"
+                            img={RotnBanner}
+                            to="/modded"
+                        />
+                        <ServerCard
+                            title="Vanilla"
+                            subtitle="Minecraft, 1.20.4"
+                            img={ModdedBanner}
+                            to="/modded"
+                        />
+                        <ServerCard
+                            title="Beta"
+                            subtitle="Minecraft, 1.7.3"
+                            img={BetaBanner}
+                            to="/modded"
+                        />
+                        <ServerCard
+                            title="Factorio"
+                            subtitle="Modded, 1.1.0"
+                            img={FactorioBanner}
+                            to="/factorio"
+                        />
                     </div>
                 </div>
             </div>
