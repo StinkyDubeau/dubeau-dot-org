62ae50ed4ece67705ad1fa1fddaa18c9b5276385
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-11 21:27:30 -0400

    Update homepage styling

--- src/components/BigButton.jsx
+++ src/components/BigButton.jsx
@@ -1,8 +1,8 @@
 export default function BigButton(props) {
     return (
-        <button className="h-16 w-16 overflow-hidden rounded-full transition-all hover:border-2 hover:border-b-4 hover:border-green-400 hover:border-b-green-700 hover:bg-green-500">
+        <button className="h-16 w-16 overflow-hidden rounded-full transition-all hover:border-2 hover:border-b-4 border-darken-200 hover:border-b-darken-300 hover:bg-darken-200">
             {props.children}
-            <p className="mt-1 font-header text-xs text-green-200">
+            <p className="mt-1 font-header text-xs text-lighten-600">
                 {props.text}
             </p>
         </button>
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,7 +1,7 @@
 export default function Panel(props) {
     return (
         <div className={props.className}>
-            <div className="max-w-lg rounded-xl border-2 border-b-4 border-green-500 border-b-green-700 border-t-green-500 bg-green-600 p-4 shadow-xl sm:mt-16">
+            <div className="max-w-lg rounded-xl border border-darken-500 border-b-darken-600 border-t-darken-500 bg-zinc-600 p-4 shadow-xl sm:mt-16">
                 {props.children}
             </div>
         </div>
