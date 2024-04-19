79d641f37044c133e913d1e2cae93b106b2f3251
StinkyDubeau <jakersunderscore09@gmail.com>
2024-04-18 20:01:26 -0400

    Add post effect for loading AI generated astro-data

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -5,7 +5,7 @@ export default function Frame(props) {
         <>
             {!props.noNavbar && <Navbar />}
 
-            <div className={`min-h-[130vh] ${props.vignette === !null ? "shadow-inner-4xl bg-zinc-800" : "bg-zinc-200"}`}>
+            <div className={`min-h-[130vh] h-max ${props.vignette === !null ? "shadow-inner-4xl bg-zinc-800" : "bg-zinc-200"}`}>
                 <div className="m-auto max-w-screen-xl p-4">
                     <div className="mt-12 flex justify-center">
                         <div className="flex h-screen flex-col">
--- src/pages/Astros.jsx
+++ src/pages/Astros.jsx
@@ -1,26 +1,7 @@
 import Frame from "../components/Frame";
 import { useState, useEffect } from "react";
 
-const astros = [
-    {
-        image: "https://cdn.discordapp.com/attachments/937152144170164286/1222245255961645236/IMG_4819.jpg?ex=66313319&is=661ebe19&hm=a09bfe70fefa7bcdcc88f9e6a3e8569cab55cb188b76d605fc382379eddd9a71&",
-        description:
-            "This is a text description that will be from 100 to 200 words of the astro.",
-        title: "Astro in the woods",
-    },
-    {
-        image: "https://media.discordapp.net/attachments/937152144170164286/1221462776636248118/IMG_4796.jpg?ex=662e5a5b&is=661be55b&hm=89b0c6bf836d4ad79550dd519d21ca6913213b6cf6a3d247a843c72c191d286c&=&format=webp&width=265&height=354",
-        description:
-            "This is a text description that will be from 100 to 200 words of the astro.",
-        title: "Astro approach",
-    },
-    {
-        image: "https://media.discordapp.net/attachments/937152144170164286/1218726850948370452/IMG_6605.png?ex=662da0d4&is=661b2bd4&hm=25fca5c4f629fdc74b93f36dbbc326a25d6b8be3d2d36e7d55023635438117cd&=&format=webp&quality=lossless&width=162&height=350",
-        description:
-            "This is a text description that will be from 100 to 200 words of the astro.",
-        title: "Google maps astro",
-    },
-];
+const astros = [];
 
 export default function Astros(props) {
     const [astros, setAstros] = useState([]);
@@ -48,7 +29,16 @@ export default function Astros(props) {
                     alt={astro.title}
                     className="rounded-lg object-contain"
                 />
-                <p className="text-darken-800">{astro.title}</p>
+                {astro.title && (
+                    <p className="text-3xl italic text-darken-800">
+                        "{astro.title}"
+                    </p>
+                )}
+                <p className="text-darken-800">
+                    Captured by @
+                    <span className="underline">{astro.photographer}</span> on{" "}
+                    {new Date(astro.timestamp).toDateString()}
+                </p>
             </div>
         );
     }
@@ -62,14 +52,20 @@ export default function Astros(props) {
                 }}
                 className="w-36 overflow-clip rounded-lg bg-lighten-800 shadow-lg transition-all hover:scale-105 hover:shadow-xl"
             >
-                
                 <img
                     src={astro.image}
                     alt={astro.title}
-                    className="h-full w-full object-cover"
+                    className="z-1 h-56 w-full object-cover"
                 />
-                <p className="z-50 text-3xl text-lighten-800">Text</p>
-
+                <div className="">
+                    {astro.title ? (
+                        <p className="text-darken-800">{astro.title}</p>
+                    ) : (
+                        <p className="font-thin italic text-darken-800">
+                            {new Date(astro.timestamp).toDateString()}
+                        </p>
+                    )}
+                </div>
             </div>
         );
     }
@@ -88,10 +84,28 @@ export default function Astros(props) {
             })
             .catch((error) => console.log(error));
     }, []);
+
+    useEffect(() => {
+        if (astro) {
+            fetch("http://localhost:3000/ask", {
+                method: "POST",
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    setAstros(data);
+                    console.log(data);
+                })
+                .catch((error) => console.log(error));
+        }
+    }, [astro]);
+
     return (
         <Frame>
             <div className="">
-                <p className="my-5 text-left font-header text-5xl text-darken-800">
+                <p className="mb-3 font-header text-5xl text-darken-800 sm:text-left">
                     Astros
                 </p>
                 {astro ? (
