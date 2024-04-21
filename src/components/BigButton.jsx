099fe3c0a5e1a7f3205979c1fd3878f0c09e9928
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-21 14:08:09 -0400

    Make homepage larger

--- src/components/BigButton.jsx
+++ src/components/BigButton.jsx
@@ -1,6 +1,6 @@
 export default function BigButton(props) {
     return (
-        <button className="h-16 w-16 overflow-hidden rounded-full transition-all hover:border-lighten-00 hover:bg-darken-200">
+        <button className="h-24 w-24 overflow-hidden rounded-full transition-all hover:border-lighten-00 hover:bg-darken-200">
             {props.children}
             <p className="mt-1 font-header text-xs text-lighten-600">
                 {props.text}
