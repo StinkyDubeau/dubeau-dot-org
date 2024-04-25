ba08d215e4acd5f99af17daf5eecdc027088933f
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-25 15:29:25 -0400

    add chat button to fun page

--- src/pages/Fun.jsx
+++ src/pages/Fun.jsx
@@ -94,6 +94,30 @@ export default function fun(props) {
                         </p>
                     </div>
                 </div>
+                <div className="w-screen">
+                    <div className="m-5">
+                        <p className="text-left font-header text-5xl text-darken-800">
+                            decentralized chat
+                        </p>
+                        <p className="text-left text-lg text-darken-800">
+                            very much a work-in-progress.
+                        </p>
+                        {/* <p className="text-left font-regular text-xl">
+                            Subtitle
+                        </p> */}
+                    </div>
+                    {/* Gradient bg */}
+                    <div className="m-5 flex animate-gradient-x flex-wrap justify-center gap-4 rounded-2xl bg-gradient-to-bl from-slate-300 via-zinc-600 to-slate-400 p-4 sm:gap-8">
+                        <Link
+                            className="flex aspect-square flex-col justify-center rounded-xl border border-x-darken-50 border-b-darken-100 border-t-lighten-100 bg-darken-200 p-2  shadow-md backdrop-blur-md transition-all hover:scale-105 hover:shadow-lg"
+                            to="/astros"
+                        >
+                            <p className="mb-1 font-header text-2xl text-lighten-800">
+                                Connect
+                            </p>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </Frame>
     );
