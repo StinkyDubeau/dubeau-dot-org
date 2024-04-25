39dd4ded12371d3eebcbcfc6c989985accbbe639
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-24 22:23:50 -0400

    Enforce prettier compliance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,8 @@ import NavButtons from "../components/NavButtons";
 export default function Home(props) {
     return (
         <div>
-            <Frame data={props.data} >noNavbar>
+            <Frame data={props.data}>
+                noNavbar>
                 <Panel className="m-4">
                     <div className="flex p-12">
                         <h1 className="font-headerScript text-6xl text-darken-700">
