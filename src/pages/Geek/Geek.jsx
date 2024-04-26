56c1e485f2d628fabdb3c3a04d4ed52b6f97464f
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-26 11:14:31 -0400

    Add log in/out to geek page. Add cookie support to geek page.

--- src/pages/Geek/Geek.jsx
+++ src/pages/Geek/Geek.jsx
@@ -5,9 +5,6 @@ import Input from "../../components/Input";
 import Card from "../../components/Card";
 
 export default function (props) {
-    const [canSubmit, setCanSubmit] = useState(false);
-    const [isLeader, setIsLeader] = useState(false);
-
     const stores = {
         928: { vans: ["022", "091", "125", "137", "163", "427", "449"] },
         940: { vans: ["124", "160", "185"] },
@@ -27,8 +24,75 @@ export default function (props) {
         "Windshield is not cracked",
     ];
 
-    const [store, setStore] = useState(stores[0]);
-    const [van, setVan] = useState(stores[0]);
+    const [store, setStore] = useState();
+    const [van, setVan] = useState();
+    const [canSubmit, setCanSubmit] = useState(false);
+    const [isLeader, setIsLeader] = useState(false);
+    const [usernameCookie, setUsernameCookie] = useState(null);
+    const [username, setUsername] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        console.log("Page loaded.");
+        // Check if user has a logged in cookie
+
+        // Example: Get the value of the 'username' cookie
+        try {
+            console.log("Trying to log in from cookies.");
+            setUsernameCookie(getCookie("username"));
+            console.log(`Username: ${username}`);
+            username !== null && setLoggedIn(true);
+        } catch {
+            console.log("User does not have a username cookie saved.");
+        }
+    }, []);
+
+    useEffect(() => {
+        updateUsername(usernameCookie);
+    }, [usernameCookie]);
+
+    const setCookie = (name, value, days) => {
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + days);
+
+        document.cookie = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
+    };
+
+    const getCookie = async (name) => {
+        const cookies = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith(`${name}=`));
+
+        return cookies ? cookies.split("=")[1] : null;
+    };
+
+    const deleteCookie = (name) => {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    };
+
+    function logOut() {
+        console.log("Logging out.");
+        deleteCookie("username");
+        setUsername(null);
+        setUsernameCookie(null);
+        setLoggedIn(false);
+    }
+
+    function updateUsername(name) {
+        setUsername(name);
+
+        // Set a username cookie that expires in 7 days
+        try {
+            console.log(`Logged in as ${name}`);
+            setCookie("username", name, 60);
+        } catch {
+            console.log(
+                "Could not save username. Check that cookies are enabled.",
+            );
+        }
+
+        setLoggedIn(true);
+    }
 
     function createQuestions(question, index) {
         return (
@@ -53,51 +117,96 @@ export default function (props) {
         );
     }
 
+    function createChecklist() {
+        return (
+            <div>
+                {/* Logo */}
+                <div className="flex h-16 justify-center">
+                    <img src="https://merchandising-assets.bestbuy.ca/bltc8653f66842bff7f/bltc645e37ea0b1a348/6183051594e50d5a63800f45/gs-logo.png" />
+                </div>
+                <div className="m-5">
+                    <p className="font-header text-5xl text-lighten-900 sm:text-left">
+                        Inspection Checklist
+                    </p>
+                </div>
+
+                {/* Gradient bg */}
+                <div className="m-5 flex animate-gradient-x justify-center rounded-3xl bg-gradient-to-tl from-orange-600 via-orange-500 to-yellow-500 p-4 sm:gap-8">
+                    <div className="flex flex-wrap justify-around gap-4 lg:gap-48">
+                        <div className="flex w-72 flex-col">
+                            <p className="font-header text-5xl text-zinc-800">
+                                Checklist
+                            </p>
+                            {/* <p className="font-header text-darken-800">
+                                    or report moderation issues
+                                </p> */}
+                        </div>
+
+                        <div className="flex flex-col gap-2">
+                            <div className="w-72 rounded-2xl bg-orange-50 p-4 shadow-xl">
+                                {questions.map(createQuestions)}
+                            </div>
+
+                            <button className="w-72 rounded-2xl bg-orange-50 p-4 font-header shadow-xl transition-all hover:scale-105">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    function createLogin() {
+        return (
+            <div className="m-5 flex animate-gradient-x justify-center rounded-3xl bg-gradient-to-tl from-orange-600 via-orange-500 to-yellow-500 p-4 sm:gap-8">
+                <p className="font-header text-xl text-lighten-800">
+                    Please log in.
+                </p>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        updateUsername(username);
+                    }}
+                >
+                    <div className="flex gap-2">
+                        <input
+                            className="w-full rounded-full bg-darken-50 p-2 shadow-inner"
+                            value={username}
+                            placeholder="Your NT login (i.e. 'jadubeau')"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <button
+                            className="w-24 rounded-full bg-darken-50 transition-all hover:bg-darken-100"
+                            type="submit"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
     return (
         <>
             <Frame data={props.data} noNavbar vignette>
                 <div className="w-screen">
-                    {/* Logo */}
-                    <div className="flex h-16 justify-center">
-                        <img src="https://merchandising-assets.bestbuy.ca/bltc8653f66842bff7f/bltc645e37ea0b1a348/6183051594e50d5a63800f45/gs-logo.png" />
-                    </div>
-                    <div className="m-5">
-                        <p className="font-header text-5xl text-lighten-900 sm:text-left">
-                            Ottawa Fleet
-                        </p>
-                    </div>
-
-                    {/* Gradient bg */}
-                    <div className="m-5 flex animate-gradient-x justify-center rounded-3xl bg-gradient-to-tl from-orange-600 via-orange-500 to-yellow-500 p-4 sm:gap-8">
-                        <div className="flex flex-wrap justify-around gap-4 lg:gap-48">
-                            <div className="flex w-72 flex-col">
-                                <p className="font-header text-5xl text-zinc-800">
-                                    Checklist
-                                </p>
-                                {/* <p className="font-header text-darken-800">
-                                    or report moderation issues
-                                </p> */}
-                            </div>
-
-                            <div className="flex flex-col gap-2">
-                                <div className="w-72 rounded-2xl bg-orange-50 p-4 shadow-xl">
-                                    {questions.map(createQuestions)}
-                                </div>
-
-                                <button className="w-72 rounded-2xl bg-orange-50 p-4 font-header shadow-xl transition-all hover:scale-105">
-                                    Submit
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    {loggedIn ? createChecklist() : createLogin()}
                 </div>
             </Frame>
             <div className="fixed bottom-0 left-0 z-50 m-0 h-16 w-screen min-w-36 bg-center sm:left-1.5 sm:top-1 sm:w-auto">
                 <div className="navbar bg-lighten-700 backdrop-blur-lg max-sm:rounded-t-xl sm:rounded-xl">
                     <div className="flex-1 px-2 lg:flex-none">
-                        <a className="text-lg font-bold">
-                            Inspection checklist
-                        </a>
+                        <div className="text-lg font-bold">
+                            <button
+                                onClick={logOut}
+                                className="bg-lighten-800 hover:bg-lighten-600"
+                            >
+                                Log out
+                            </button>
+                            {username && username.toString()}
+                        </div>
                     </div>
                     <div className="flex flex-1 justify-end px-2">
                         <div className="flex items-stretch gap-2">
