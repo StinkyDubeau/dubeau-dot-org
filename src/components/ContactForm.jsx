8d97a3a5747405f53c82137304dfc7ff82ed7a4f
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-25 21:01:39 -0400

    Fix dark mode contact form and add additional placeholder text

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -92,7 +92,7 @@ export default function ContactForm(props) {
             );
         } else {
             return (
-                <div className="flex flex-col justify-center h-full">
+                <div className="flex h-full flex-col justify-center">
                     <p className="font-header text-xl text-darken-700">
                         Your message was sent.
                     </p>
@@ -109,13 +109,13 @@ export default function ContactForm(props) {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 16 16"
                         fill="currentColor"
-                        className="h-4 w-4 opacity-70"
+                        className="h-4 w-4 text-darken-800 opacity-70"
                     >
                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                     </svg>
                     <input
                         type="text"
-                        className="grow bg-transparent"
+                        className="grow bg-transparent text-darken-800"
                         placeholder="Name"
                         value={username}
                         onChange={(e) => {
@@ -129,14 +129,14 @@ export default function ContactForm(props) {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 16 16"
                         fill="currentColor"
-                        className="h-4 w-4 opacity-70"
+                        className="h-4 w-4 text-darken-800 opacity-70"
                     >
                         <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                         <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                     </svg>
                     <input
                         type="text"
-                        className="grow bg-transparent"
+                        className="grow bg-transparent text-darken-800"
                         placeholder="Email"
                         value={email}
                         onChange={(e) => {
@@ -146,8 +146,8 @@ export default function ContactForm(props) {
                 </label>
 
                 <textarea
-                    className="rounded-xl bg-darken-50  p-2"
-                    placeholder="Message"
+                    className="rounded-xl bg-darken-50  p-2 text-darken-800"
+                    placeholder="Message (Drag bottom-right corner for more space)"
                     value={body}
                     onChange={(e) => {
                         setBody(e.target.value);
@@ -155,7 +155,7 @@ export default function ContactForm(props) {
                 ></textarea>
 
                 <button
-                    className="h-10 rounded-xl bg-darken-100 shadow transition-all hover:rounded-lg hover:bg-darken-50"
+                    className="h-10 rounded-xl bg-darken-100 font-header text-darken-600 shadow transition-all hover:rounded-lg hover:bg-darken-50"
                     onClick={handleSubmit}
                 >
                     Submit
