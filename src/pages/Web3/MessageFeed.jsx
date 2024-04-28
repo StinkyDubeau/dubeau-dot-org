ea0503f203ec963b5661f03de4eacdb88b47ec2f
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-28 17:03:39 -0400

    Get good responsiveness, finally.

--- src/pages/Web3/MessageFeed.jsx
+++ src/pages/Web3/MessageFeed.jsx
@@ -22,7 +22,7 @@ export default function MessageFeed(props) {
     }
 
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 py-2">
             {messages[0] ? (
                 messages.map(createMessage)
             ) : (
