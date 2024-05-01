46d2acf125c5c0bd1ec4cd63c95f24226f08ca83
StinkyDubeau <jakersunderscore09@gmail.com>
2024-05-01 13:34:53 -0400

    Optimize for really long nicks, soas to not break the app

--- src/pages/Web3/MessageFeed.jsx
+++ src/pages/Web3/MessageFeed.jsx
@@ -13,18 +13,18 @@ export default function MessageFeed(props) {
         return (
             <div
                 key={index + message.from.id}
-                className="justify-left scrollbar-hide flex justify-between gap-2 overflow-y-auto overflow-x-scroll rounded-3xl bg-darken-50 px-4 py-2"
+                className="justify-left flex-0 scrollbar-hide flex justify-between gap-2 overflow-y-auto overflow-x-scroll rounded-3xl bg-darken-50 px-4 py-2"
             >
                 {message.from.nick && (
-                    <p className="my-auto max-h-96 text-left text-lg text-darken-800 font-semibold max-sm:text-sm">
-                        {message.from.nick}
+                    <p className="my-auto max-w-36 max-h-8 overflow-scroll scrollbar-hide text-left text-lg font-semibold text-darken-800 max-sm:text-sm">
+                        {message.from.nick}:
                     </p>
                 )}
-                <p className="scrollbar-hide my-auto max-h-96 overflow-scroll text-left text-lg text-darken-800 max-sm:text-sm">
+                <p className="scrollbar-hide my-auto max-h-96 flex-1 overflow-scroll text-left text-lg text-darken-800 max-sm:text-sm">
                     {message.content}
                 </p>
                 {/* User ID and Time are hidden on small displays */}
-                <div className="flex h-full flex-col justify-center max-sm:hidden">
+                <div className="flex-0 flex h-full flex-col justify-center max-sm:hidden">
                     <p className="text-sm text-darken-500">
                         {message.timestamp.toLocaleString()}
                     </p>
