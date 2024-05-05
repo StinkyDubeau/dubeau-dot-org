2f07aad3ba845520a677e2d08518fe29d5f806f5
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-05-05 12:44:14 -0400

    Add more fonts. Remove panel shadow

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,8 +1,9 @@
 export default function Panel(props) {
     return (
         <div className={props.className}>
-            <div className="max-w-md animate-gradient-x rounded-3xl bg-white shadow-lg">
+            <div className="max-w-md animate-gradient-x rounded-3xl bg-white">
                 {props.children}
+                {/* 𝐒wag */}
             </div>
         </div>
     );
