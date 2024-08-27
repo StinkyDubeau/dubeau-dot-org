7a77b6d7e6dfc8210d3f684076bfaacbce735ddc
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-08-27 17:25:12 -0400

    Work on navbar and server connection pages. Add blog page

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -12,6 +12,12 @@ export default function NavButtons(props) {
             </Link>
             <Link
                 className="m-1 rounded-xl p-1 font-header text-2xl text-darken-700 transition-all hover:rounded-3xl hover:bg-darken-50 hover:px-3 hover:shadow"
+                to="/blog"
+            >
+                <p className="mt-0.5">blog</p>
+            </Link>
+            <Link
+                className="m-1 rounded-xl p-1 font-header text-2xl text-darken-700 transition-all hover:rounded-3xl hover:bg-darken-50 hover:px-3 hover:shadow"
                 to="/fun"
             >
                 <p className="mt-0.5">fun</p>
--- src/pages/Servers/Vanilla.jsx
+++ src/pages/Servers/Vanilla.jsx
@@ -3,19 +3,18 @@ import Frame from "../../components/Frame";
 import Button from "../../components/Button";
 import { Link } from "react-router-dom";
 
-
 export default function fun(props) {
     return (
         <Frame data={props.data}>
             <div>
-            <div className="w-xl">
+                <div className="w-xl">
                     <div className="m-5">
                         <p className="text-left font-header text-5xl text-darken-800">
                             Vanilla
                         </p>
 
                         <p className="text-left font-regular text-xl">
-                        An invite-only 1.21 minecraft server community
+                            An invite-only 1.21 minecraft server community
                         </p>
                     </div>
                     {/* Gradient bg */}
@@ -23,6 +22,26 @@ export default function fun(props) {
                         <p className="text-left font-header text-3xl text-darken-800">
                             How to join
                         </p>
+                        <p className="text-left font-header text-xl text-darken-800">
+                            1. You must be whitelisted to play!
+                        </p>
+                        <div className="flex w-full gap-2">
+                            {/* Easy Method */}
+                            <div className="flex-1 rounded-xl bg-lighten-800">
+                                <p>nutse</p>
+                            </div>
+                            {/* Reccomended Method */}
+                            <div className="flex-1 rounded-xl bg-lighten-800">
+                                <p>balls</p>
+                            </div>
+                        </div>
+                        <p className="text-left font-header text-xl text-darken-800">
+                            > The easy way
+                        </p>
+                        <p className="text-left font-header text-xl text-darken-800">
+                            > The reccomended way
+                        </p>
+
                         <p className="text-left text-lg text-darken-800">
                             Simple method
                         </p>
