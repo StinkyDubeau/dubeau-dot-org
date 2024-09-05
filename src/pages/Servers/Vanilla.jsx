b347acd672c35a629845fafe7d53d3d1b3fd5e04
StinkyDubeau <jakersunderscore09@gmail.com>
2024-09-05 17:02:41 -0400

    Work on vanilla page setup instructions

--- src/pages/Servers/Vanilla.jsx
+++ src/pages/Servers/Vanilla.jsx
@@ -11,7 +11,7 @@ export default function fun(props) {
 
     function createUser(user) {
         return (
-            <div className="text-white hover:text-darken-300">
+            <div className="scale-100 text-white transition-all hover:scale-110 hover:text-darken-300">
                 <p className="font-header text-lg text-darken-800">
                     {user.name}
                 </p>
@@ -24,14 +24,13 @@ export default function fun(props) {
         <Frame data={props.data}>
             <div>
                 <div className="w-xl flex flex-col gap-6">
-                    <div className="mt-5 flex bg-white rounded-3xl p-2 gap-12 overflow-clip">
+                    <div className="mt-5 flex gap-12 overflow-clip rounded-3xl bg-white p-2">
                         <div className="flex-0 flex max-w-96 justify-center p-2">
-                            <div className="flex flex-col justify-center align-middle gap-12">
+                            <div className="flex flex-col justify-center gap-12 align-middle">
                                 <div className="flex flex-col gap-2">
                                     <p className="text-center font-header text-5xl text-darken-800">
                                         Vanilla
                                     </p>
-
 
                                     <p className="text-center text-xl font-light text-darken-800">
                                         An invite-only 1.21 minecraft community,
@@ -47,7 +46,7 @@ export default function fun(props) {
                                         <div className="flex max-w-96 justify-center">
                                             <Marquee
                                                 speed="150"
-                                                className="mt-6 w-full"
+                                                className="mt-6 w-full overflow-clip"
                                             >
                                                 {whitelist.map(createUser)}
                                             </Marquee>
@@ -78,17 +77,49 @@ export default function fun(props) {
                         </p>
 
                         <p className="text-left font-header text-xl text-darken-800">
-                            Simply launch the latest version of Minecraft and
-                            connect to{" "}
+                            0. Connect with us on{" "}
+                            <span className=" scale-100 transition-all hover:scale-125">
+                                <Link
+                                    className="font-bold text-darken-800"
+                                    to="/contact"
+                                >
+                                    discord
+                                </Link>{" "}
+                            </span>
+                            to be whitelisted.
+                        </p>
+
+                        <p className="text-left font-header text-xl text-darken-800">
+                            1. Launch the latest version of Minecraft Java
+                            Edition.
+                        </p>
+                        <p className="text-left font-header text-xl text-darken-800">
+                            2. Connect to{" "}
                             <span className="font-light text-red-500">
                                 dubeau.ddns.net
                             </span>
-                            . Follow posted signs for general rules.
+                        </p>
+
+                        <p className="text-left font-header text-xl text-darken-800">
+                            3. Have fun and be cool! Follow posted signs for
+                            general rules.
+                        </p>
+                    </div>
+                    <div className="rounded-3xl bg-lighten-800 p-4">
+                        <p className="text-left font-header text-3xl text-darken-800">
+                            Modded Client
+                        </p>
+
+                        <p className="text-left font-header text-xl text-darken-800">
+                            1. Download our official{" "}
+                            <span className="font-light text-red-500">
+                                MultiMC instance
+                            </span>
                         </p>
                     </div>
                 </div>
             </div>
-            <p className="font-header text-darken-800">
+            <p className="mt-6 font-header text-darken-800">
                 Last updated September 5th, 2024
             </p>
         </Frame>
