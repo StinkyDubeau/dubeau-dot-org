63e10575dfe2d05c9aff9942fc4d67356636eb55
StinkyDubeau <jakersunderscore09@gmail.com>
2024-09-13 11:21:00 -0400

    Work on counter page

--- src/pages/Toys/Counter.jsx
+++ src/pages/Toys/Counter.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import BigButton from "../../components/BigButton";
-import Button from "../../components/Button";
 import Frame from "../../components/Frame";
 import { Link } from "react-router-dom";
 
 export default function (props) {
-
     const [count, setCount] = useState(0);
 
     return (
@@ -13,25 +10,22 @@ export default function (props) {
             <Frame
                 noNavbar
                 data={props.data}
-
             >
-                <div className="bg-pink-600 h-screen w-screen text-lighten-800 text-xl">
-                    <h1 className="mb-8 font-header text-3xl font-light tracking-tighter text-darken-700 max-sm:text-5xl">
-                        Under Construction
-                    </h1>
+                <div className="bg-pink-600 flex flex-col h-screen w-screen text-lighten-800 text-xl p-6">
+                    <div className="flex h-full flex-col justify-center">
+                        <h1 className="font-headerScript text-pink-200 text-9xl">
+                            {count}
+                        </h1>
 
-                    <h1 className="text-lighten-800">
-                        {count}
-                    </h1>
+                        <div className="flex max-sm:flex-col justify-center gap-2">
+                            <button className="p-6 min-w-36 min-h-36 text-6xl font-header bg-pink-500 active:bg-pink-100 hover:bg-pink-300 active:scale-95 rounded-xl transition-all hover:scale-105" onClick={() => setCount(count + 1)}>+</button>
+                            <button className="p-6 min-w-36 min-h-36 text-7xl font-header bg-pink-500 hover:bg-pink-300 active:bg-pink-100 active:scale-95 rounded-xl transition-all hover:scale-105" onClick={() => setCount(count - 1)}>-</button>
+                        </div>
 
-                    <div className="flex justify-center gap-2">
-                        <button className="p-6 bg-pink-500 hover:bg-pink-300 rounded-xl transition-all hover:scale-110" onClick={() => setCount(count + 1)}>+</button>
-                        <button className="p-6 bg-pink-500 hover:bg-pink-300 rounded-xl transition-all hover:scale-110" onClick={() => setCount(count - 1)}>-</button>
                     </div>
-
                     <Link
                         to="/fun"
-                        className="text-lighten-800 underline"
+                        className="text-pink-800 font-header m-0"
                     >
                         Back to home
                     </Link>
