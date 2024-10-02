c4ac74b864dcb5511c3789df6824988591cc945a
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-10-02 10:50:46 -0400

    Add future hobby options. Add back to home button on p2p chat login page

--- src/pages/Fun.jsx
+++ src/pages/Fun.jsx
@@ -53,7 +53,7 @@ export default function fun(props) {
                             Game servers
                         </p>
                         <p className="text-left text-lg text-darken-600">
-                            These servers are are invite-only. Join our{" "}
+                            Most servers are are invite-only. Join our{" "}
                             <Link
                                 to="/contact"
                                 className="underline underline-offset-1"
@@ -129,6 +129,14 @@ export default function fun(props) {
                         >
                             <p className="mb-1 font-header text-2xl text-lighten-800">
                                 Guitar
+                            </p>
+                        </Link>
+                        <Link
+                            className="min-w-24 flex aspect-square flex-col justify-center rounded-xl border border-x-darken-50 border-b-darken-100 border-t-lighten-100 bg-darken-200 p-2  shadow-md backdrop-blur-md transition-all hover:scale-105 hover:shadow-lg"
+                            to="/fun/tabs"
+                        >
+                            <p className="mb-1 font-header text-2xl text-lighten-800">
+                                Photography
                             </p>
                         </Link>
                         <Link
