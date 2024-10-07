71b3449957e8671d8be5184b8f93525663b48d5a
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-10-07 13:14:41 -0400

    Mess with S3

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { Amplify } from 'aws-amplify';
+import awsconfig from './amplifyconfiguration.json';
+console.log("Configuring amplify in index.js")
+Amplify.configure(awsconfig);
--- src/pages/Toys/Gallery.jsx
+++ src/pages/Toys/Gallery.jsx
@@ -1,29 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Frame from "../../components/Frame";
 import { Link } from "react-router-dom";
 
-export default function (props) {
-    const [count, setCount] = useState(0);
+import { list } from "aws-amplify/storage";
 
+try {
+    const result = await list({
+        path: "Backgrounds/",
+        // Alternatively, path: ({identityId}) => `protected/${identityId}/photos/`
+    });
+} catch (error) {
+    console.log(error);
+}
+
+export default function (props) {
     return (
         <>
             <Frame
                 noNavbar
                 data={props.data}
             >
-                <div className="flex flex-col h-screen w-screen text-darken-800 text-xl p-6">
+                <div className="flex h-screen w-screen flex-col p-6 text-xl text-darken-800">
                     <div className="flex h-full flex-col justify-center">
-                        <h1 className="font-header text-darken-200 text-9xl">
+                        <h1 className="font-header text-3xl text-darken-200">
                             Gallery under construction
                         </h1>
-
-
                     </div>
-                    <image className="bg-blue-300 h-24 w-24" src="s3://dubeau-dot-org/Backgrounds/aleksey-parakhnevich-E6EoRgfMOok-unsplash.jpg" />
-                    <image className="bg-red-300 h-24 w-24" src="https://static.vecteezy.com/system/resources/previews/022/448/291/large_2x/save-earth-day-poster-environment-day-nature-green-ai-generative-glossy-background-images-tree-and-water-free-photo.jpg" />
+                    <image
+                        className="h-24 w-24 bg-blue-300"
+                        src="s3://dubeau-dot-org/Backgrounds/aleksey-parakhnevich-E6EoRgfMOok-unsplash.jpg"
+                    />
+                    <image
+                        className="h-24 w-24 bg-red-300"
+                        src="https://static.vecteezy.com/system/resources/previews/022/448/291/large_2x/save-earth-day-poster-environment-day-nature-green-ai-generative-glossy-background-images-tree-and-water-free-photo.jpg"
+                    />
                     <Link
                         to="/fun"
-                        className="text-darken-800 font-header m-0"
+                        className="m-0 font-header text-darken-800"
                     >
                         Back to fun
                     </Link>
