00ab367b55013071c2ec9d8aa58c45c390041173
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-11-10 13:54:00 -0500

    Fix servercard rendering in dark theme

--- src/components/ServerCard.jsx
+++ src/components/ServerCard.jsx
@@ -14,13 +14,13 @@ export default function ServerCard(props) {
                     {/* Container */}
                     <div className="z-25 borde relative -top-[95%] m-2 flex h-40 flex-col justify-center gap-2 rounded-lg drop-shadow transition-all hover:-top-full">
                         {/* Spacer */}
-                        <p className="z-10 mt-4 font-header text-3xl text-lighten-900 drop-shadow">
+                        <p className="z-10 mt-4 font-header text-3xl text-white drop-shadow">
                             {props.title}
                         </p>
-                        <p className="z-10 font-header font-semibold text-lighten-800 drop-shadow">
+                        <p className="z-10 font-header font-semibold text-white drop-shadow">
                             {props.subtitle}
                         </p>
-                        <p className="animate-pulse font-header text-lighten-800 hover:animate-none">
+                        <p className="animate-pulse font-header text-white hover:animate-none">
                             {props.announcement && props.announcement}
                         </p>
                     </div>
