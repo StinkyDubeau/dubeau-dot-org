a520e030b7e86e95b5f43e8be7f2786df3006733
Jake Dubeau <jakersunderscore09@gmail.com>
2024-12-11 07:52:08 -0500

    Continue work on UI mockup for refactoring of Idle game. Work on prettier tooling for better tailwind formatting

--- src/pages/Toys/Idle/Components/UIMockup.jsx
+++ src/pages/Toys/Idle/Components/UIMockup.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import InnerCard from "./InnerCard";
 import OuterCard from "./OuterCard";
 
 export default function UIMockup(props) {
+    const [focus, setFocus] = useState(0); // 0: Capacitors, 1: Generators, 2: Loads
+
     return (
         <div className="font-regular text-darken-800">
             <p className="text-darken-800">UI Mockup</p>
@@ -11,6 +14,7 @@ export default function UIMockup(props) {
                     id="totals"
                     className="flex-0 flex flex-col gap-2 p-2"
                 >
+                    <p>Network Summary</p>
                     <InnerCard className="aspect-square bg-green-100">
                         <h1>Total Capacitance</h1>
                         <h2>{0}</h2>
@@ -27,11 +31,12 @@ export default function UIMockup(props) {
 
                 <div
                     id="paginated-area"
-                    className="flex-1 p-2"
+                    className="flex flex-1 flex-col gap-2 p-2"
                 >
+                    <p>Focus</p>
                     <InnerCard
                         id="test-page"
-                        className="h-full w-full bg-darken-100"
+                        className="h-max w-max bg-darken-100"
                     >
                         <p>This is a test page.</p>
                     </InnerCard>
