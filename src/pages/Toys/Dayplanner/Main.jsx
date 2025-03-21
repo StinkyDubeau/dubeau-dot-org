17499d2f06ee7ffe8d0a9a51f7e12668563b923f
Jake Dubeau <jakersunderscore09@gmail.com>
2025-03-21 11:19:35 -0400

    Work on tasks structure

--- src/pages/Toys/Dayplanner/Main.jsx
+++ src/pages/Toys/Dayplanner/Main.jsx
@@ -12,6 +12,12 @@ import { v4 as uuidv4 } from "uuid";
 // Weekly tasks: Daily tasks that the user can promote to weekly recurring tasks:
 // - A weekly task has a target "completions count", which is set at the time of promotion.
 
+// Data handling:
+// - Store tasks in cookies or local storage.
+// - Load tasks from cookies or local storage.
+// - Save tasks to a database, or share as plain text.
+// - Load tasks from a database, or import from plain text.
+
 export default function Dayplanner(props) {
     const [dailyTasks, setDailyTasks] = useState([
         {
@@ -28,13 +34,13 @@ export default function Dayplanner(props) {
     // In minutes, how much time to dedicate to the day's tasks.
     const [dayDuration, setDayDuration] = useState(8 * 60);
 
-    function DayMaker() {
+    function RenderDailyTasks() {
         // This component visualizes a day of tasks, taking into consideration the duration of the "day duration", which can be set or changed by the user.
         return (
             <>
                 <p>There are {dayDuration / 60} hours to fill today.</p>
                 {dailyTasks.map((task, index) => (
-                    <Task
+                    <RenderTask
                         key={task.uuid}
                         name={task.name}
                         description={task.description}
@@ -48,43 +54,45 @@ export default function Dayplanner(props) {
         );
     }
 
-    function TaskMaker() {
+    function RenderTaskForm() {
         // name, description, creationDate, colour, uuids
         const [name, setName] = useState("");
         const [description, setDescription] = useState("");
         const [creationDate, setCreationDate] = useState(new Date());
-        const [colour, setColour] = useState("bg-red-400");
+        const [colour, setColour] = useState("");
         const [uuid, setUuid] = useState(uuidv4());
 
         return (
-            <div className="rounded bg-lighten-800 p-2 shadow">
+            <div className="flex gap-2 rounded border-none bg-lighten-800 p-2 shadow max-sm:flex-col">
                 {/* NAME */}
                 <input
                     type="text"
                     placeholder="Name"
-                    className="rounded bg-lighten-600 p-1"
+                    className="rounded border-none bg-darken-50 p-1 shadow-inner"
                     onChange={(e) => setName(e.target.value)}
                 />
                 {/* DESCRIPTION */}
                 <input
                     type="text"
                     placeholder="Description"
-                    className="rounded bg-lighten-600 p-1"
+                    className="rounded border-none bg-darken-50 p-1 shadow-inner"
                     onChange={(e) => setDescription(e.target.value)}
                 />
                 {/* COLOUR */}
                 <select
-                    className="rounded bg-lighten-600 p-1"
+                    className="w-grow rounded border-none bg-lighten-600 p-1 shadow"
                     onChange={(e) => setColour(e.target.value)}
+                    defaultValue="bg-yellow-400"
                 >
-                    <option value="bg-red-400">Red</option>
+                    <option value="bg-yellow-400">Yellow</option>
                     <option value="bg-green-400">Green</option>
+                    <option value="bg-red-300">Red</option>
                     <option value="bg-blue-400">Blue</option>
-                    <option value="bg-purple-400">Purple</option>
+                    <option value="bg-purple-500">Purple</option>
                 </select>
 
                 <button
-                    className="rounded bg-lighten-600 p-1"
+                    className="rounded bg-lighten-600 p-1 shadow"
                     onClick={() => {
                         // Add task to dailyTasks
                         setDailyTasks([
@@ -109,7 +117,14 @@ export default function Dayplanner(props) {
         );
     }
 
-    function Task({ index, name, description, creationDate, colour, uuid }) {
+    function RenderTask({
+        index,
+        name,
+        description,
+        creationDate,
+        colour,
+        uuid,
+    }) {
         const [showDetails, setShowDetails] = useState(false);
 
         return (
@@ -139,10 +154,10 @@ export default function Dayplanner(props) {
                         className="my-auto flex flex-col gap-2 rounded bg-lighten-800 p-2"
                     >
                         {" "}
-                        <DayMaker />
+                        <RenderDailyTasks />
                     </div>
 
-                    <TaskMaker />
+                    <RenderTaskForm />
                 </div>
             </Frame>
         </>
