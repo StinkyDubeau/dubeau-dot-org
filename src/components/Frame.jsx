e257c3d826e0af05e63d9a0bb7baa21a83a4f7e4
Jake Dubeau <jakersunderscore09@gmail.com>
2025-03-21 20:49:32 -0400

    Animate presence of experimental flag and loading bar

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Frame(props) {
     function ScrollToTop() {
@@ -14,41 +15,48 @@ export default function Frame(props) {
         return (
             <div>
                 {/* Experimental flag */}
-                {props.data.experimental && (
-                    <div className="fixed bottom-0 z-50 mx-auto flex w-full justify-between bg-red-500 pl-2 ">
-                        <div className="flex w-full justify-stretch gap-2">
-                            <p className="flex-0 overflow-ellipsis text-nowrap border font-header text-white">
-                                Experimental features are active.
-                            </p>
-                            <Link
-                                onClick={() => {
-                                    props.setData({
-                                        ...props.data,
-                                        experimental: false,
-                                    });
-                                }}
-                                to="/"
-                                className="flex-1 text-nowrap font-header text-white underline"
-                            >
-                                Disable
-                            </Link>
-                            <Link
-                                to="/trackers"
-                                className="flex-1 text-nowrap font-header text-white underline"
-                            >
-                                Go to trackers page
-                            </Link>
+                <AnimatePresence>
+                    {props.data.experimental && (
+                        <motion.div
+                            initial={{ height: 0 }}
+                            animate={{ height: "auto" }}
+                            exit={{ height: 0 }}
+                            className="fixed bottom-0 z-50 mx-auto flex w-full justify-between bg-red-500 pl-2 "
+                        >
+                            <div className="flex w-full justify-stretch gap-2">
+                                <p className="flex-0 overflow-ellipsis text-nowrap font-header text-white">
+                                    Experimental features are active.
+                                </p>
+                                <Link
+                                    onClick={() => {
+                                        props.setData({
+                                            ...props.data,
+                                            experimental: false,
+                                        });
+                                    }}
+                                    to="/"
+                                    className="flex-0 m-0.5 text-nowrap rounded bg-lighten-800 font-header text-sm text-darken-800 shadow transition-all hover:scale-105 hover:shadow-lg"
+                                >
+                                    Disable
+                                </Link>
+                                <Link
+                                    to="/trackers"
+                                    className="flex-0 m-0.5 text-nowrap rounded bg-lighten-800 font-header text-sm text-darken-800 shadow transition-all hover:scale-105 hover:shadow-lg"
+                                >
+                                    Go to trackers page
+                                </Link>
 
-                            <p className="flex-0 overflow-ellipsis text-nowrap border text-right font-header text-white">
-                                Session data:
-                            </p>
-                            <p className="flex-0 scroll-px-20 overflow-scroll text-nowrap font-header text-white shadow-inner-xl scrollbar-hide">
-                                {props.data &&
-                                    Object.values(props.data).toString()}
-                            </p>
-                        </div>
-                    </div>
-                )}
+                                <p className="flex-0 ml-auto overflow-ellipsis text-nowrap text-right font-header text-white">
+                                    Session data:
+                                </p>
+                                <p className="flex-0 overflow-scroll text-nowrap px-2 font-header text-white shadow-inner-xl scrollbar-hide">
+                                    {props.data &&
+                                        Object.values(props.data).toString()}
+                                </p>
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
         );
     }
