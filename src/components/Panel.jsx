99722918bec37b7c8c672efec0d4624e06a35570
Jake Dubeau <jakersunderscore09@gmail.com>
2025-04-08 21:50:38 -0400

    Work on darkmode homepage. Completely different vibegit add .

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -4,17 +4,20 @@ export default function Panel(props) {
     return (
         <AnimatePresence>
             <motion.div
-                initial={{ scale: 0.5 }}
-                whileInView={{ scale: 1 }}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
                 className={
                     props.className
                         ? props.className
-                        : `rounded-3xl bg-lighten-900 p-2 shadow-lg`
+                        : `relative top-0 h-full w-full animate-gradient-x rounded-3xl bg-gradient-to-bl from-pink-700 via-purple-600 to-zinc-200 shadow-inner-3xl shadow-lighten-600`
                 }
             >
-                {props.children}
+                <div className="border border-x-darken-50 border-b-darken-50 border-t-darken-100 bg-lighten-400 opacity-95 shadow-lg backdrop-blur-lg transition-all hover:border-x-darken-100 hover:border-b-darken-100 hover:bg-lighten-300 hover:opacity-100">
+                    {props.children}
+                </div>
                 {/* 𝐒wag */}
             </motion.div>
+            {/* Gradient background, only if no higher order styling */}
         </AnimatePresence>
     );
 }
