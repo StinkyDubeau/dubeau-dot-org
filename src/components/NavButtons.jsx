5bc37a244d0033498e9a8050c09b34d68e1e3180
Jake Dubeau <jakersunderscore09@gmail.com>
2025-04-08 22:00:17 -0400

    work on goofy lil dark mode

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -6,7 +6,7 @@ export default function NavButtons(props) {
         <div className="mx-auto flex">
             {!props.noHome && (
                 <Link
-                    className="m-1 rounded-xl p-1 font-header text-2xl text-darken-700 transition-all hover:rounded-3xl hover:bg-darken-50 hover:px-3 hover:shadow"
+                    className="flex-0 m-1 rounded-xl p-1 font-header text-2xl text-darken-700 transition-all hover:flex-1 hover:rounded-3xl hover:bg-darken-50 hover:px-3 hover:shadow"
                     to="/"
                 >
                     <p className="mt-0.5">home</p>
@@ -19,7 +19,7 @@ export default function NavButtons(props) {
                 <p className="mt-0.5">playground</p>
             </Link>
             <Link
-                className="m-1 rounded-xl p-1 font-header text-2xl text-darken-700 transition-all hover:rounded-3xl hover:bg-darken-50 hover:px-3 hover:shadow"
+                className="flex-0 m-1 rounded-xl p-1 font-header text-2xl text-darken-700 transition-all hover:flex-1 hover:rounded-3xl hover:bg-darken-50 hover:px-3 hover:shadow"
                 to="/contact"
             >
                 <p className="mt-0.5">contact</p>
