af67b765d8951d3c215754274ddfe07827d1bc64
Jake Dubeau <jakersunderscore09@gmail.com>
2025-07-18 21:24:54 -0400

    Remove pointer cursor on homepage buttons, for vibes

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -6,27 +6,27 @@ export default function NavButtons({ isOnHome, children }) {
         <div className="mx-auto flex h-full gap-3">
             {isOnHome ? (
                 <Link
-                    className="m-auto flex h-full w-full flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
+                    className="m-auto flex h-full w-full cursor-default flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
                     to="/chat"
                 >
                     <p className="text-nowrap">p2p chat</p>
                 </Link>
             ) : (
                 <Link
-                    className="m-auto flex h-full w-full flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
+                    className="m-auto flex h-full w-full cursor-default flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
                     to="/"
                 >
                     <p className="">home</p>
                 </Link>
             )}
             <Link
-                className="m-auto flex h-full w-full flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
+                className="m-auto flex h-full w-full cursor-default flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
                 to="/fun"
             >
                 <p className="">playground</p>
             </Link>
             <Link
-                className="m-auto flex h-full w-full flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
+                className="m-auto flex h-full w-full cursor-default flex-col justify-center rounded-full font-header text-2xl text-darken-700 transition-all hover:bg-lighten-900 hover:px-3 hover:shadow-lg"
                 to="/contact"
             >
                 <p className="">contact</p>
